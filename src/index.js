d1b1d2eca415c7ab284e9d516918293309af2fc6
Jonathan Cavalcante <jonathancmpc@gmail.com>
2020-06-16 09:41:20 -0300

    Teste

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express(); /* Aplicação criada */
 
 /* Definindo a rota */
 app.get('/', (request, response) => {
-    return response.send('Hello World'); /* o send retorna um texto */
+    return response.send('Hello Worl'); /* o send retorna um texto */
 });
 
 app.listen(3333); /* Definindo a porta onde o node/backend será executada */
